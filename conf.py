1f73ef9f9a3f5ebab26ac2aeee00e409629d8b7c
PEmugi <PEmugi@chizuwota.net>
2021-09-05 23:01:48 +0900

    Nikola auto commit.
    Nikola version: 8.1.3

--- conf.py
+++ conf.py
@@ -1036,13 +1036,13 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, intensedebate, isso, muut, commento, utterances
 # You can leave this option blank to disable comments.
-# COMMENT_SYSTEM = "disqus"
+COMMENT_SYSTEM = ""
 # COMMENT_SYSTEM = "facebook"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-# COMMENT_SYSTEM_ID = "chizuwota"
+COMMENT_SYSTEM_ID = "chizuwota"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
